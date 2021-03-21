c32f2798bf2abb9930a254936a062ebd962eed37
JohsonRodriguez <sukerjc@gmail.com>
2021-03-20 19:21:44 -0600

    run

---
+++ src/main/java/heroku/superkiddos/SuperkiddosApplication.java
@@ -0,0 +1,23 @@
+package heroku.superkiddos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SuperkiddosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperkiddosApplication.class, args);
+	}
+
+}
+
+@RestController
+class HelloController{
+	@GetMapping("/")
+	String hello(){
+		return "Hello World";
+	}
+}
---
+++ src/test/java/heroku/superkiddos/SuperkiddosApplicationTests.java
@@ -0,0 +1,13 @@
+package heroku.superkiddos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperkiddosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
